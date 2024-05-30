adf036a21fc12e6b4b35ba85a48351274512758d
Jake <122070716+HEqux@users.noreply.github.com>
2024-05-29 22:12:05 -0700

    made the site worse

--- scripts/skibidAudio.js
+++ scripts/skibidAudio.js
@@ -1,5 +1,6 @@
-function clickSkibid(){
-    var sound = document.getElementById('audio');
-    sound.volume = 0.2;
-    sound.play()
+function clickSkibid() {
+    // Create a new audio element
+    var newSound = new Audio('./audio/skibidi-toilet.mp3');
+    newSound.volume = 0.2;
+    newSound.play();
 }
